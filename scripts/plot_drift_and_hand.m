d2dae8ab0032a22730cdca42de9f510877360150
Ivar Clemens <post@ivarclemens.nl>
2015-06-11 17:22:55 +0100

    Example plot script.

---
+++ scripts/plot_drift_and_hand.m
@@ -0,0 +1,23 @@
+clear all;
+
+data = load('../data/eric.mat');
+
+t_drift = [data.log.time_drift];
+drift = [data.results.proprioceptive_drift];
+offset = [data.results.offset];
+hands = zeros(numel(t_drift), 3);
+
+for i = 1:numel(t_drift)
+    [~, I] = min(abs(data.hands{i}(:, 1) - t_drift(i)));
+    hands(i, :) = data.hands{i}(I, 2:end);
+end
+
+
+%plot(drift, '.-');
+%plot(offset, hands(:, 1), 'x');
+subplot(1, 2, 1);
+plot(hands(2:end, 1) / 10 + 1, '.-');
+subplot(1, 2, 2);
+plot(drift(2:end) * 100 + 1, '.-');
+
+[offset' hands(:,1)]
