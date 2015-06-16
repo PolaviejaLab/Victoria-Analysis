f0436bef24c819f9e2780548c5011dc82426e16f
Ivar Clemens <post@ivarclemens.nl>
2015-06-16 11:58:46 +0100

    Plot drift and hand position

--- scripts/plot_drift_and_hand.m
+++ scripts/plot_drift_and_hand.m
@@ -1,24 +1,69 @@
-clear all;
-dataDirectory = 'C:\Users\User\Documents\MATLAB\ExperimentDiscontinuity';
-cacheDirectory = dataDirectory;
-data = load([cacheDirectory '/mert.mat']);
+function plot_drift_and_hand()
+    global config_;
 
-t_drift = [data.log.time_drift];
-drift = [data.results.proprioceptive_drift];
-offset = [data.results.offset];
-hands = zeros(numel(t_drift), 3);
+    n_participants = size(config_.participants, 1);
+    n_conditions = numel(config_.conditionsShort);    
+    
+    % Construct two matrices, one for drift and the other for hand position
+    %  participant x condition
 
-for i = 1:numel(t_drift)
-    [~, I] = min(abs(data.hands{i}(:, 1) - t_drift(i)));
-    hands(i, :) = data.hands{i}(I, 2:end);
-end
+    drift = nan(n_participants, n_conditions);
+    handp = nan(n_participants, n_conditions);
+    
+    for i_participant = 1:n_participants
+        % Load data for participant
+        participant = lower(config_.participants{i_participant, 1});
+        data = load(fullfile(config_.cacheDirectory, [participant '.mat']));
+        
+        t_drift = [data.log.time_drift];
+        
+        drift(i_participant, :) = [data.results.proprioceptive_drift];
+        offset = [data.results.offset];
+                
+        for i_condition = 1:n_conditions
+            % Remove nans
+            invalid = any(isnan(data.hands{i_condition}), 2);
+            data.hands{i_condition}(invalid, :) = [];
+            
+            [~, I] = min(abs(data.hands{i_condition}(:, 1) - t_drift(i_condition)));
+            
+            dt = (data.hands{i_condition}(I, 1) - t_drift(i_condition)) * 3600 * 24 * 1000;
+            
+            if(dt > 100)
+                warning('No sample found within 10ms (%f)', dt);                
+            else
+                handp(i_participant, i_condition) = data.hands{i_condition}(I, 2);
+            end
+        end        
+    end
 
+    
+    drift(:, 1) = [];
+    handp(:, 1) = [];
+    
+    for i_participant = 1:n_participants
+        subplot(2, 4, i_participant); cla; hold on;
+        plot(drift(i_participant, 1:3) * 100, 'bo-');
+        plot(handp(i_participant, 1:3) / 10, 'rx-');
 
-%plot(drift, '.-');
-%plot(offset, hands(:, 1), 'x');
-subplot(1, 2, 1);
-plot(hands(2:end, 1) / 10 + 1, '.-');
-subplot(1, 2, 2);
-plot(drift(2:end) * 100 + 1, '.-');
-
-[offset' hands(:,1)]
+        plot(4:6, drift(i_participant, 4:6) * 100, 'bo-');
+        plot(4:6, handp(i_participant, 4:6) / 10, 'rx-');
+        
+        plot([0 5 10], 'k--');
+        plot(4:6, [0 5 10], 'k--');
+        
+        legend({'Drift', 'HandP'});
+        xlabel('Condition');
+        ylabel('Position (cm)');
+        
+        set(gca, 'XTick', 1:6);
+        set(gca, 'XTickLabel', config_.conditionsShort(2:end));
+        
+        title(config_.participants{i_participant, 1});
+    end
+%     subplot(1, 2, 1);
+%     plot(hands(2:end, 1) / 10 + 1, '.-');
+%     subplot(1, 2, 2);
+%     plot(drift(2:end) * 100 + 1, '.-');
+% 
+%     [offset' hands(:,1)]
