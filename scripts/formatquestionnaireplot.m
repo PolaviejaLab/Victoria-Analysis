9c17c8191052cf66ad6641a5432046718a914827
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2015-08-13 10:45:27 +0100

    Formatting questionnaire plot

--- scripts/formatquestionnaireplot.m
+++ scripts/formatquestionnaireplot.m
@@ -1,21 +1,42 @@
-function [colordots] = formatquestionnaireplot (condition, type_question)
+function [colordots] = formatquestionnaireplot (condition, ...
+    type_question, question_number)
 
-    if type_question == 1
-            colordots = 'r';
-        elseif type_question == 2
-            colordots = 'm';
-        elseif type_question == 3
-            colordots = 'b';
-        elseif type_question == 4
-            colordots = 'c';
-        elseif type_question == 5
-            colordots = 'g';
-        end
+rectangle_height = 8;
+
+
+
+line ([0.5  14.4], [4 4], 'color', [0.827451 0.827451 0.827451], ...
+    'linestyle', ':');
+
+if type_question == 1
+        rectangle ('position', [(question_number - 1) 0 1 rectangle_height], ...
+        'facecolor', [252 212 197]/256, 'linestyle', 'none');
+    colordots = 'r';
+
+elseif type_question == 2
+    colordots = 'm';
+        rectangle ('position', [(question_number - 1) 0 1 rectangle_height], ...
+        'facecolor', [253 237 207]/256, 'linestyle', 'none');
+elseif type_question == 3
+    colordots = 'b';
+            rectangle ('position', [(question_number - 1) 0 1 rectangle_height], ...
+        'facecolor', [225 237 249]/256, 'linestyle', 'none');
+elseif type_question == 4
+    colordots = 'c';
+     rectangle ('position', [(question_number - 1) 0 1 rectangle_height], ...
+        'facecolor', [206 208 233]/256, 'linestyle', 'none');
+elseif type_question == 5
+    colordots = 'g';
+    rectangle ('position', [(question_number - 1) 0 1 rectangle_height], ...
+        'facecolor', [221 238 212]/256, 'linestyle', 'none');
+end
+
 
 ylim ([0 8]);
 set(gca, 'YTick', 1:7);
 xlim ([0 15]);
 set (gca, 'XTick', 1:14);
+
 xlabel ('Question Number');
 if condition == 1
     title ('Continuous Limb, Offset = 0 cm');
@@ -30,5 +51,5 @@ elseif condition == 5
 elseif condition == 6
     title ('Discontinuous Limb, Offset = 10 cm');
 else
-    title ('Unkown Condition'); 
+    title ('Unkown Condition');
 end
--- scripts/plotquestionnaireanswers.m
+++ scripts/plotquestionnaireanswers.m
@@ -12,21 +12,25 @@ for i = 1:numel(conditions)
     i_condition = conditions(i);
     
     figure (1), % boxplot + scatter plot with separated dots
+    
     subplot (2, numel(conditions)/2, i);
     for i_question = 1:size(cube, 1)
         type_question = questions(i_question);
+        
+        [colordots] = formatquestionnaireplot (i_condition, ...
+            type_question, i_question);
+        
         for i_subject = 1:size(cube, 3)
-            data_vector(i_subject) = cube(i_question, i_condition, i_subject);
+            data_vector(i_subject) = cube(i_question, i_condition, ...
+                i_subject);
         end
         
         condition_matrix (i_question, :) = data_vector;
         
-        [colordots] = formatquestionnaireplot (i_condition, type_question);
-        
         scatter (((i_question-0.02*length(data_vector)) + ...
-            (1:length(data_vector))*0.04), data_vector, colordots); hold on      
+            (1:length(data_vector))*0.04), data_vector, colordots); hold on
     end
     
     boxplot (condition_matrix', 'colors', 'k');
-     
+    
 end
