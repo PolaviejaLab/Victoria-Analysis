63cad670191cc2eef3ce28faa46ff53afbbe679a
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2016-01-19 14:29:25 +0000

    New Source Data Location

--- scripts/config_local.m
+++ scripts/config_local.m
@@ -1,4 +1,5 @@
 function config_local()
     global config_;
     
-    config_.dataDirectory = 'C:\Users\User\Google Drive\01. PhD\#. ExperimentDiscontinuity\SourceData';
+    config_.dataDirectory = 'E:\Google Drive\01. PhD\#. ExperimentDiscontinuity\SourceData';
+    
