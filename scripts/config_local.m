94005265d4d31de6cad7952dfaa8a44e68226404
vbrugada <victoria.brugada@neuro.fchampalimaud.org>
2016-05-30 18:25:46 +0100

    changed the location of the data

--- scripts/config_local.m
+++ scripts/config_local.m
@@ -1,5 +1,5 @@
 function config_local()
     global config_;
     
-    config_.dataDirectory = 'E:\Google Drive\01. PhD\#. ExperimentDiscontinuity\SourceData';
+    config_.dataDirectory = 'E:\Google Drive\01. PhD\#1 ExperimentDiscontinuity\SourceData';
     
